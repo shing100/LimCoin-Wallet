520d0ec66715d895fa2557a2bc52c0dcc374c3cb
DESKTOP-CN9OS9T\shing <shing100@naver.com>
2018-07-10 21:05:11 +0900

    create-react-app and set ENV=dev Script 수정 및 실행환경 구성

--- main.js
+++ main.js
@@ -6,19 +6,30 @@ const { app, BrowserWindow } = electron;
 
 let mainWindow;
 
-function createWindow() {
+const createWindow = () => {
     mainWindow = new BrowserWindow({
-      width:800,
-      height: 600
+      width: 800,
+      height: 600,
+      resizable: false
     });
 
-    mainWindow.loadURL(
-      url.format({
-        pathname: path.join(__dirname,"index.html"),
-        protocol: "file",
-        slashes: true
-      })
-    );
-}
+    const ENV = process.env.ENV;
+
+    if(ENV === "dev"){
+      mainWindow.loadURL("http://localhost:3000");
+    }else{
+      mainWindow.loadURL(
+        url.format({
+          pathname: path.join(__dirname,"./build/index.html"),
+          protocol: "file",
+          slashes: true
+        })
+      );
+    }
+
+    mainWindow.on("closed", () => {
+      mainWindow = null;
+    });
+};
 
 app.on("ready", createWindow);
